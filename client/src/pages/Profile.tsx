e3ab80766c65a0f12c1ffbbea8ffd2c1fb61e1d1
PaulPintang <pauljpintang@gmail.com>
2023-02-22 11:23:26 +0800

    feat: change profile picture(frontend)

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,20 +1,81 @@
-import { Container, Card, Button, Title } from "@mantine/core";
+import { useState } from "react";
+import Avatar from "react-avatar-edit";
+
+import {
+  Container,
+  Center,
+  Button,
+  Title,
+  Stack,
+  FileButton,
+  Avatar as AvatarMantine,
+  Text,
+  Modal,
+} from "@mantine/core";
 import { useNavigate } from "react-router-dom";
+import avatar from "../assets/user.png";
 
 const Profile = () => {
+  const [opened, setOpened] = useState<boolean>(false);
+  const [src, setSrc] = useState("");
+  const [profile, setProfile] = useState<string | null>(null);
+  const [viewImg, setViewImg] = useState<string | null>(null);
   const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.clear();
     navigate("/");
   };
 
-  return (
-    <Container mt={200}>
-      <Card p="lg" radius="sm" withBorder>
-        <Title order={1}>HELLO {localStorage.getItem("user")}</Title>
+  const onCrop = (view: any) => {
+    setViewImg(view);
+  };
 
-        <Button onClick={handleLogout}>Sign out</Button>
-      </Card>
+  const onClose = () => {
+    setViewImg(null);
+  };
+
+  const saveImage = () => {
+    setProfile(viewImg);
+    setOpened(false);
+  };
+
+  console.log(profile);
+
+  return (
+    <Container>
+      <Center style={{ width: "100%", height: "100vh" }}>
+        <Stack>
+          <Center>
+            <AvatarMantine
+              radius={100}
+              size={200}
+              src={profile ? profile : avatar}
+              onClick={() => setOpened(true)}
+            />
+          </Center>
+          <Title order={1}>{localStorage.getItem("user")}</Title>
+          <Button onClick={handleLogout}>Sign out</Button>
+        </Stack>
+        <Modal
+          centered
+          opened={opened}
+          onClose={() => setOpened(false)}
+          title="Update profile!"
+        >
+          <div>
+            <Avatar
+              onClose={onClose}
+              onCrop={onCrop}
+              width={390}
+              height={295}
+              src={src}
+            />
+            <Button mt={20} onClick={saveImage}>
+              Upload
+            </Button>
+          </div>
+        </Modal>
+      </Center>
     </Container>
   );
 };
